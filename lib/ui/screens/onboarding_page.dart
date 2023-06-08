693c4e40ef20179a8b7009d617203f1808663f41
burzza <bineshburzaa@gmail.com>
2023-06-08 22:38:23 +0545

    making gradients

--- lib/ui/screens/onboarding_page.dart
+++ lib/ui/screens/onboarding_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guffadis_chat/ui/config/app_colors.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({Key? key}) : super(key: key);
@@ -13,7 +14,19 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: AppColors.darkPrimary,
+          gradient: RadialGradient(
+            tileMode: TileMode.clamp,
+            colors: [
+              AppColors.primaryColor,
+              AppColors.darkPrimary,
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
