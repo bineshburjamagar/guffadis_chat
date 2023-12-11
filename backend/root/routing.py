bc7d8486e7adae1e05c5c2a165f016cafa913c41
burzza <bineshburzaa@gmail.com>
2023-12-11 11:32:04 +0545

    user cred models

--- backend/root/routing.py
+++ backend/root/routing.py
@@ -2,6 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from chat.consumers import ChatRoomConsumer
+from django.core.asgi import get_asgi_application
 
 # URLs that handle the WebSocket connection are placed here.
 # websocket_urlpatterns=[
@@ -11,7 +12,10 @@ from chat.consumers import ChatRoomConsumer
 #                 ]
 
 application = ProtocolTypeRouter( 
+
     {
+
+        "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(
             URLRouter(
                [
--- backend/root/settings.py
+++ backend/root/settings.py
@@ -105,9 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
+    
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
@@ -130,11 +128,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR  
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "chat.User"
 
 
 
