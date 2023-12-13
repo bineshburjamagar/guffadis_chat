bf04cdb40805eb76a5126b4790fe0951c7f03622
niteshneupane <nitesh.neupane@asparksys.com>
2023-12-13 15:36:57 +0545

    added simplejwt

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ChatRoomList, MessageList,GetToken
+from .views import ChatRoomList, MessageList, GetCallToken
 
 urlpatterns = [
-    path('',  ChatRoomList.as_view(),name='room_list'),
-    path('message/',  MessageList.as_view(),name='messages'),
-    path('call-token/',  GetToken.as_view(),name='call-token'),
-
-       
+    path("", ChatRoomList.as_view(), name="room_list"),
+    path("message/", MessageList.as_view(), name="messages"),
+    path("call-token/", GetCallToken.as_view(), name="call-token"),
 ]
--- backend/chat/views.py
+++ backend/chat/views.py
@@ -3,30 +3,48 @@ from rest_framework.views import APIView
 from .models import ChatRoom, Message
 from .serializers import ChatRoomSerializer, MessageSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from livekit import api as liveKitApi
+from django.conf import settings
+
 
 class ChatRoomList(ListAPIView):
-
     serializer_class = ChatRoomSerializer
 
     def get_queryset(self):
         q = ChatRoom.objects.all()
-        
-
         return q
+
 
 class MessageList(ListAPIView):
     serializer_class = MessageSerializer
-    
+
     def get_queryset(self):
         gg = Message.objects.all()
         return gg
 
 
-class GetToken(APIView):
+class GetCallToken(APIView):
+    permission_classes = [IsAuthenticated,]
     def get(self, request, *args, **kwargs):
-        data = {
-            "call_token":"Hello"
-        }
-        return Response(
-            data
-        )
+        try:
+            api_key =settings.LIVEKIT_API_KEY 
+            api_secret =settings.LIVEKIT_API_SECRET 
+            user = request.user
+            if user:
+                token = (
+                    liveKitApi.AccessToken(api_key=api_key, api_secret=api_secret)
+                    .with_identity(f"user-{user.id}-{user.first_name}")
+                    .with_name(f"{user.first_name} {user.last_name}")
+                    .with_grants(
+                        liveKitApi.VideoGrants(
+                            room_join=True,
+                            room="room1",
+                        ),
+                    )
+                )
+                return Response({"token": token.to_jwt()})
+            return Response({"message": "Not Authorized"}, status=400)
+        except Exception as e:
+            return Response({"message": str(e)}, status=400)
